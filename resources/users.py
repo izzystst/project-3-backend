379fdb4d5a675f5d02b0dadf59c98a0416a63324
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-04-30 13:39:12 -0500

    login works woo

--- resources/users.py
+++ resources/users.py
@@ -1,9 +1,9 @@
 import models 
 
 from flask import Blueprint, request, jsonify
-from flask_bcrypt import generate_password_hash
+from flask_bcrypt import generate_password_hash, check_password_hash
 from playhouse.shortcuts import model_to_dict
-from flask_login import login_user
+from flask_login import login_user, current_user
 
 users = Blueprint('users', 'users')
 
@@ -59,3 +59,50 @@ def register():
 				message="registered a user woohoo!",
 				status=201
 				), 201
+@users.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	payload['email']=payload['email'].lower()
+	payload['username'] = payload['username'].lower()
+
+	try:
+		user = models.User.get(models.User.email==payload['email'])
+		user_dict = model_to_dict(user)
+
+		checked_password = check_password_hash(user_dict['password'], payload["password"])
+		if(checked_password):
+			login_user(user)
+			print(f"{current_user.username} is current_user.username in POST login")
+			user_dict.pop('password')
+
+			return jsonify(
+				data=user_dict,
+				message=f"loghed in as {user_dict['email']}",
+				status=200
+				), 200
+		else: 
+			print('bad pword ')
+			return jsonify(
+				data={},
+				message="email or password is wrong",
+				status=401
+				), 401
+
+	except models.DoesNotExist:
+		print('bad username')
+		return jsonify(
+			data={},
+			message="email or password is wrong",
+			status=401
+			), 401
+
+
+
+
+
+
+
+
+
+
+
