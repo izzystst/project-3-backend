c27fba35f58950a808b4973b9e56365a2627786f
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-04-30 09:36:18 -0500

    hello world

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 DEBUG=True
 PORT=8000
-
-
 app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello():
+	return 'hell world!'
+
+
 
 if __name__ == '__main__':
 	app.run(debug=DEBUG, port=PORT)
