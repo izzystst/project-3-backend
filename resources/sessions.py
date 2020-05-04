400a873087350e7e97b971455f9de808199790cb
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-05-03 21:03:38 -0500

    maybe got my assanas groupped

--- resources/sessions.py
+++ resources/sessions.py
@@ -44,12 +44,34 @@ def create_session():
 def sessions_index():
 	sessions = models.Session.select()
 	sessions_dict = [model_to_dict(session) for session in sessions]
-	print(sessions_dict)
-	asanas_in_sessions = models.SessionPoses.select()
-	print('this is the asana in the session')
-	print(asanas_in_sessions)
+	# pp.pprint(sessions_dict)
+	asanas_in_sessions = models.SessionPoses.select().order_by('session')
+	# print('this is the asana in the session')
+	# pp.pprint(asanas_in_sessions)
 	asanas_in_session_dict = [model_to_dict(asanas_in_session) for asanas_in_session in asanas_in_sessions]
+	print("these are the asanas in dict")
 	pp.pprint(asanas_in_session_dict)
+	# sessions_dict.update(asanas_in_session_dict)
+	# pp.pprint(session_dict)
+	id_list = []
+	final_list = []
+	for asana in asanas_in_session_dict:
+		current_id = 1
+		# print(asana['session']['id'])
+		# if asana if matches curent id, push to id list, if not put the id list , in final list, as a list, empty list, checnage current id of the one that didnt match 
+		if(asana['session']['id'] == current_id):
+			id_list.append(asana)
+		else:
+			final_list.append(id_list)
+			id_list = []
+			current_id = asana['session']['id']
+
+
+	pp.pprint(final_list)
+			# print("this is the asanas in session dict")
+			# make final list a list dictionaries - probably not worth it  
+		# ?sessions_dict[asanas] = final list
+	# pp.pprint(asanas_in_session_dict)
 	return jsonify(
 		data={'session': sessions_dict, 'asanas': asanas_in_session_dict},
 		message="found all sessions and the poses in them",
